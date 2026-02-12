ac4527ec1f45e500506c20bc83a9b0ae8da5d090
Jeevlin <princessjeev@gmail.com>
2026-02-12 23:58:04 +0530

    Add delete category API

--- backend-server/controllers/categoryC.js
+++ backend-server/controllers/categoryC.js
@@ -35,4 +35,21 @@ const getCategory= async(req,res)=>{
 
     }
 }
-module.exports={addCategory,getCategory}
+const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await CategorySchema.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Category deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to delete category",
+    });
+  }
+};
+
+module.exports={addCategory,getCategory,deleteCategory}
--- backend-server/routes/categoryRoute.js
+++ backend-server/routes/categoryRoute.js
@@ -5,5 +5,6 @@ const categoryC = require('../controllers/categoryC')
 
 router.post('/addCategory',categoryC.addCategory)
 router.get('/getCategory',categoryC.getCategory)
+router.delete('/deleteCategory/:id', categoryC.deleteCategory)  
 
 module.exports=router
